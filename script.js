6ee500ae57518ec2d5cce98fcc426aa148b06a9c
bulbul1434 <bulbultomar14@gmail.com>
2024-11-13 14:55:39 +0530

    commit

---
+++ script.js
@@ -0,0 +1,3 @@
+document.getElementById('resumeButton').addEventListener('click', function() {
+    window.open("https://drive.google.com/file/d/1F-IwmizDWr6qNf5ATimfbtKgICVebA7P/view?usp=sharing", "_blank");
+});
