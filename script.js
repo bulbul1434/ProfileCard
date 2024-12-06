5d2bb5ff40034c435512e9bee230252a9d7b66dd
bulbul1434 <bulbultomar14@gmail.com>
2024-12-06 22:35:49 +0530

    commited

--- script.js
+++ script.js
@@ -1,3 +1,3 @@
 document.getElementById('resumeButton').addEventListener('click', function() {
-    window.open("https://drive.google.com/file/d/1F-IwmizDWr6qNf5ATimfbtKgICVebA7P/view?usp=sharing", "_blank");
+    window.open("https://drive.google.com/file/d/12iOY0PuiZcF3RUzeXcEqYfSz2D-ZLHde/view?usp=sharing", "_blank");
 });
